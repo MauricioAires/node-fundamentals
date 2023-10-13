b8207e5b8a23ebb65bffd5e00186b8f545767abd
MauricioAires <mauricioaires015@gmail.com>
2023-10-13 09:40:32 -0300

    feat(server) node watch

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  return res.end("Hello world!!");
+});
+
+server.listen(3333, () => {
+  console.log(`Server listening on 3333`);
+});
